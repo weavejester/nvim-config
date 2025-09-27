10fbbbfad4beb65745e91e840cd2d89067f5b685
James Reeves <jreeves@weavejester.com>
2025-09-27 19:15:13 +0100

    Add noice plugin

---
+++ config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,6 @@
+-- [nfnl] fnl/plugins/noice.fnl
+local function _1_()
+  local noice = require("noice")
+  return noice.setup({lsp = {override = {["vim.lsp.util.convert_input_to_markdown_lines"] = true, ["vim.lsp.util.stylize_markdown"] = true, ["cmp.entry.get_documentation"] = true}}, presets = {bottom_search = true, command_palette = true, long_message_to_split = true, inc_rename = false, lsp_doc_border = false}})
+end
+return {{"folke/noice.nvim", event = "VeryLazy", dependencies = {"MunifTanjim/nui.nvim", "rcarriga/nvim-notify"}, config = _1_}}
