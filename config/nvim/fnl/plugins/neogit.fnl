24ad93614f387d6265a3cfa28a8dad3bd4b2b4c1
James Reeves <jreeves@weavejester.com>
2025-09-30 18:53:19 +0100

    Add Neogit plugin

---
+++ config/nvim/fnl/plugins/neogit.fnl
@@ -0,0 +1,6 @@
+[{1 :NeogitOrg/neogit
+  :dependencies [:nvim-lua/plenary.nvim
+                 :sindrets/diffview.nvim
+                 :nvim-telescope/telescope.nvim]
+  :opts
+  {:graph_style :unicode}}]
---
+++ config/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,2 @@
+-- [nfnl] fnl/plugins/neogit.fnl
+return {{"NeogitOrg/neogit", dependencies = {"nvim-lua/plenary.nvim", "sindrets/diffview.nvim", "nvim-telescope/telescope.nvim"}, opts = {graph_style = "unicode"}}}
