ac6a37af044c392e7e61a2aa3472b1a62d96d564
James Reeves <jreeves@weavejester.com>
2025-09-30 19:06:44 +0100

    Add key mapping to Neogit

--- config/nvim/fnl/plugins/neogit.fnl
+++ config/nvim/fnl/plugins/neogit.fnl
@@ -2,5 +2,8 @@
   :dependencies [:nvim-lua/plenary.nvim
                  :sindrets/diffview.nvim
                  :nvim-telescope/telescope.nvim]
-  :opts
-  {:graph_style :unicode}}]
+  :keys [{1 "<leader>gg"
+          2 (fn [] (let [neogit (require "neogit")] (neogit.open)))
+          :desc "Open Neogit"}]
+  :opts {:graph_style :unicode
+         :kind :floating}}]
--- config/nvim/lua/plugins/neogit.lua
+++ config/nvim/lua/plugins/neogit.lua
@@ -1,2 +1,6 @@
 -- [nfnl] fnl/plugins/neogit.fnl
-return {{"NeogitOrg/neogit", dependencies = {"nvim-lua/plenary.nvim", "sindrets/diffview.nvim", "nvim-telescope/telescope.nvim"}, opts = {graph_style = "unicode"}}}
+local function _1_()
+  local neogit = require("neogit")
+  return neogit.open()
+end
+return {{"NeogitOrg/neogit", dependencies = {"nvim-lua/plenary.nvim", "sindrets/diffview.nvim", "nvim-telescope/telescope.nvim"}, keys = {{"<leader>gg", _1_, desc = "Open Neogit"}}, opts = {graph_style = "unicode", kind = "floating"}}}
